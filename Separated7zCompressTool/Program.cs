e2a2630d3240dc14d0b72242c084f59c7d88ec62
xlfdll <xlfdll@gmail.com>
2019-01-05 14:22:31 -0800

    Version 1.0: Initial Release

---
+++ Separated7zCompressTool/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Separated7zCompressTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Separated 7-Zip Compression Tool");
+            Console.WriteLine("(C) 2019 Xlfdll Workstation");
+            Console.WriteLine();
+
+            if (args.Length > 0)
+            {
+                Boolean keepOriginal = (args.Length > 3 && args[2] == "/nodel");
+                String command = @"C:\Program Files\7-Zip\7z.exe";
+                String arguments = @"a -mx {del} ""{0}"" ""{1}""".Replace("{del}", keepOriginal ? String.Empty : "-sdel");
+
+                foreach (String fileName in Directory.GetFiles(args[0], args[1], SearchOption.TopDirectoryOnly))
+                {
+                    String newFileName = Path.Combine(args[0], Path.GetFileNameWithoutExtension(fileName) + ".7z");
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo(command, String.Format(arguments, newFileName, fileName))
+                    {
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false
+                    };
+
+                    using (Process process = Process.Start(processStartInfo))
+                    {
+                        Console.WriteLine(process.StandardOutput.ReadToEnd());
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(process.StandardError.ReadToEnd());
+                        Console.ResetColor();
+
+                        process.WaitForExit();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine();
+                Console.WriteLine("Separated7zCompressTool <Directory> <Search Pattern>");
+                Console.WriteLine();
+            }
+        }
+    }
+}
